abfbf96fc01fd440f7356ae670fd457c4a37687f
Bastien Arlot <arlot.b@gmail.com>
2022-04-21 14:10:00 +0200

    insert method done, toString need more accuracy

--- script.js
+++ script.js
@@ -1,7 +1,66 @@
 class Node {
-constructor(data, leftChild, rightChild) {
+constructor(data, leftChild = null, rightChild = null) {
     this.data = data;
-    this.leftChild = null;
-    this.rightChild = null;
+    this.leftChild = leftChild;
+    this.rightChild = rightChild;
 }
 }
+
+class Tree {
+    constructor() {
+        this.root = null;
+
+    }
+    toString = () =>{
+        if(this.root){
+            console.log(this.root.data);
+            if((this.root.leftChild !== null) && (this.root.rightChild !== null)){
+                console.log(this.root.leftChild.data);
+                console.log(this.root.rightChild);
+            }else if(this.root.leftChild){
+                console.log(this.root.leftChild.data);
+            }else if(this.root.rightChild){
+                console.log(this.root.rightChild.rightChild.data);
+            }
+        }
+    };
+   insert = (data) => {
+        const node = this.root;
+        if(node === null){
+            this.root = new Node(data);
+            return;
+        }else{
+            const searchTree = (node) =>{
+                if(data < node.data){
+                    if(node.leftChild === null){
+                        node.leftChild = new Node(data);
+                        return;
+                    }else if(node.leftChild !== null){
+                        return searchTree(node.leftChild);
+                    }
+                }else if(data > node.data){
+                    if(node.rightChild === null){
+                        node.rightChild = new Node(data);
+                        return;
+                    }else if(node.rightChild !== null){
+                        return searchTree(node.rightChild);
+                    }
+                }else{
+                    return null;
+                }
+            };return searchTree(node);
+        }
+   }
+}
+
+const bst = new Tree();
+
+bst.insert(2);
+bst.insert(5);
+bst.insert(8);
+bst.insert(9);
+bst.insert(3);
+bst.insert(1);
+
+console.log(bst.root);
+bst.toString();
